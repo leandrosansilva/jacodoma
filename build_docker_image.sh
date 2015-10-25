d49e41af6e9e77654cd63a981f5da696a7747b31
Leandro Santiago <leandrosansilva@gmail.com>
2015-10-25 21:28:29 +0100

    Build in a separate step is alwais a good idea

---
+++ build_docker_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t leandrosansilva/jacodoma -f docker_image/Dockerfile .
--- run_on_docker.sh
+++ run_on_docker.sh
@@ -4,8 +4,6 @@ set -e
 
 JACODOMA_SOURCE="$PWD"
 
-docker build -t leandrosansilva/jacodoma -f docker_image/Dockerfile .
-
 docker run --rm -it -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$JACODOMA_SOURCE":/gopath/src/jacodoma leandrosansilva/jacodoma \
